a8dc9f77ad24054990a1a122aff37978ed00730f
Dakshay Patel <32039107+dakshaypatel@users.noreply.github.com>
2022-08-11 08:29:44 -0400

    updating controller

--- app/url_info_controller.py
+++ app/url_info_controller.py
@@ -4,9 +4,9 @@ import json
 
 app = Flask(__name__)
 
-@app.route( '/urlinfo/1/<hostname_and_port>/<original_path_and_query_string>')
-def get_url_information(hostname_and_port, original_path_and_query_string):
-    malware_info = get_url_info(hostname_and_port)
+@app.route('/urlinfo/1/<hostname>/<path>')
+def get_url_information(hostname, path):
+    malware_info = get_url_info(hostname)
     if not malware_info:
         malware_info = "NOT_MALWARE"
     return malware_info
